8293d6e0dd665348e311be6b8a6346a884559930
Vincent <vincentvonschmidt@gmail.com>
2021-06-04 17:03:36 +0200

    modified:   linie.py 	modified:   ../Test/TasterTest/TasterTest.py 	../Test/Motor Test/Linie.py

--- Hauptprogramm/linie.py
+++ Hauptprogramm/linie.py
@@ -6,45 +6,46 @@ LEDW = 18
 LEDR = 19
 LEDG = 20
 
-TASTER_L = machine.Pin(16 , machine.Pin.IN)
-TASTER_R = machine.Pin(17 , machine.Pin.IN)
+TASTER_L = machine.Pin(16 , machine.Pin.IN, machine.Pin.PULL_DOWN)
+TASTER_R = machine.Pin(17 , machine.Pin.IN, machine.Pin.PULL_DOWN)
+
 
 s = Sensor(LEDW)
 V = 50
 
-if TASTER_R.value(1): 
-    print("Kalibrieren ... ")
-    #s.setMinMax(20, 6000, 20, 6000)
-    s.kalibrierStart()
+print("Taste R --> Kalibrieren, losfahren")
+print("Taste L --> losfahre")
+print("keine Taste nach 5 sek. --> losfahren")
 
-    #print(utime.ticks_ms())
+tasteL = TASTER_L.value()
+tasteR = TASTER_R.value()
+
+t1 = utime.ticks_ms()
+while (utime.ticks_ms() - t1 < 5000) & (tasteL == 0) & (tasteR == 0):
+    tasteL = TASTER_L.value()
+    tasteR = TASTER_R.value()
+    
+if tasteR == 1:
+    print("Kalibrieren ... ")
+    s.kalibrierStart()
+    
     for i in range(500): 
         s.kalibrierRunde()
         utime.sleep_ms(10)
-    #print(utime.ticks_ms())
-    #print(s.miniL, s.maxiL, s.miniR, s.maxiR)
 
     print("Kalibrieren fertig!")
 
-if TASTER_L.value(1):
-    print("Start ...")
 
-#print(utime.ticks_ms())
-for i in range(10000): 
+utime.sleep_ms(200)
+ 
+print("Fahre los ... Taste L --> stop")
+
+while not TASTER_L.value() == 1:
     s.messen()
-#print(utime.ticks_ms())
-
-while True:
-    s.messen()
-    if TASTER_L.value(1) or TASTER_R.value(1):
-        #Dose umfahren
-        print("Test")
-    else: 
-        diff = s.wertR - s.wertL
-        diff = diff * 5
-        #print(diff)
-        OnFwd(MOT_A, V + diff)
-        OnFwd(MOT_B, V - diff)
-    
-
-
+    diff = s.wertR - s.wertL
+    diff = diff * 5
+    #print(diff)
+    OnFwd(MOT_A, V + diff)
+    OnFwd(MOT_B, V - diff)
+Off(MOT_AB)
+print("ENDE")
